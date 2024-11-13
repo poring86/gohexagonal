f34f1cbd909117da3dbe4a094da0576ad0acffd8
poring86 <matheuslino86a@gmail.com>
2024-11-13 14:30:32 -0400

    first commit

---
+++ application/product.go
@@ -0,0 +1,52 @@
+package application
+
+type ProductInterface interface {
+	IsValid() (bool, error)
+	Enable() error
+	Disable() error
+	GetID() string
+	GetName() string
+	GetStatus() string
+	GetPrice() float64
+	ChangePrice(price float64) error
+}
+
+const (
+	DISABLED = "disabled"
+	ENABLED  = "enabled"
+)
+
+type Product struct {
+	ID     string
+	Name   string
+	Price  float64
+	Status string
+}
+
+func (p *Product) IsValid() (bool, error) {
+
+}
+
+func (p *Product) Enable() error {
+
+}
+
+func (p *Product) Disable() error {
+
+}
+
+func (p *Product) GetID() string {
+
+}
+
+func (p *Product) GetName() string {
+
+}
+
+func (p *Product) GetStatus() string {
+
+}
+
+func (p *Product) GetPrice() float64 {
+
+}
